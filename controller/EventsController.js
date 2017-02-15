5b512798f590ca725d6405b4cc8ecc45c5e4dd54
Bhuvana <bhuvanatn@gmail.com>
2017-02-15 16:12:06 +1100

    refactored the eventdetails page for voting events

--- controller/EventsController.js
+++ controller/EventsController.js
@@ -6,11 +6,11 @@ MyApp.controller('EventsController', function EventsController($scope, eventData
       .then(function(event) { $scope.event = event; console.log(event);})
       .catch(function(response){ console.log(response); });
 
-  $scope.count=0;
-  $scope.upVoteCount = function(){
-    $scope.count++;
+
+  $scope.downVoteSession = function(session){
+    session.upVoteCount--;
   };
-  $scope.downVoteCount = function(){
-    $scope.count--;
+  $scope.upVoteSession = function(session) {
+    session.upVoteCount++;
   };
 });
