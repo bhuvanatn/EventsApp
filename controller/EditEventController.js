c1e6bd46eabf69d7bd668b31924d99e075d01b24
Bhuvana <bhuvanatn@gmail.com>
2017-02-16 15:21:20 +1100

    form validation using

--- controller/EditEventController.js
+++ controller/EditEventController.js
@@ -1,12 +1,20 @@
-MyApp.controller("EditEventController", function($scope, eventData){
-$scope.title ="Add New Event";
-$scope.saveEvent = function(event){
-  eventData.save(event)
-            .$promise
-            .then(function(response){console.log('Success..', response);})
-            .catch(function(response){console.log('Failure..', response);});
-};
-$scope.cancelEdit = function(){
-  window.location = "/EventDetails.html";
-};
+MyApp.controller("EditEventController", function($scope, eventData) {
+    $scope.title = "Add New Event";
+    $scope.saveEvent = function(event, newEventForm) {
+              if(newEventForm.$valid) {
+            window.alert("saved clicked");
+            // eventData.save(event)
+            //     .$promise
+            //     .then(function(response) {
+            //         // alert("you clicked saved");
+            //         console.log('Success..', response);
+            //     })
+            //     .catch(function(response) {
+            //         console.log('Failure..', response);
+            //     });
+        }
+    };
+    $scope.cancelEdit = function() {
+        window.location = "/EventDetails.html";
+    };
 });
