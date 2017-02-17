28ac4506e017390caaf084e661df6c9703366b6f
Bhuvana <bhuvanatn@gmail.com>
2017-02-18 00:44:07 +1100

    appended markup to div using compile service

---
+++ controller/CompileSampleController.js
@@ -0,0 +1,7 @@
+MyApp.controller('CompileSampleController',
+    function CompileSampleController($scope, $compile) {
+        $scope.appendDivToElement = function(markup) {
+            return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
+        };
+    }
+);
