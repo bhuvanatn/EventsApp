e414dea28f8f671ef55adac8f6cfd0736aa242a9
Bhuvana <bhuvanatn@gmail.com>
2017-02-18 14:35:12 +1100

    parse service example

--- controller/CompileSampleController.js
+++ controller/CompileSampleController.js
@@ -1,5 +1,24 @@
 MyApp.controller('CompileSampleController',
-    function CompileSampleController($scope, $compile) {
+    function CompileSampleController($scope, $compile, $parse) {
+      console.log("$parse", $parse);
+      console.log("$compile", $compile);
+      var fn = $parse("1 +2");
+      console.log(fn());
+
+      var getter = $parse("event.name");
+
+      var context1 = {event: {name:'Angular Boot Camp'}};
+      var context2 = {event: {name:'Code Camp'}};
+
+      console.log(getter(context1));
+      console.log(getter(context2));
+
+      console.log(getter(context1, context2));
+
+      var setter = getter.assign;
+      setter(context2, 'Code Retreat');
+
+      console.log(context2.event.name);
         $scope.appendDivToElement = function(markup) {
             return $compile(markup)($scope).appendTo(angular.element("#appendHere"));
         };
