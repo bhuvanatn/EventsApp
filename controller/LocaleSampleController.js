25ad1786fa07b0ac18d4ba9c1727308044c007a9
Bhuvana <bhuvanatn@gmail.com>
2017-02-18 15:25:56 +1100

    locale service example

---
+++ controller/LocaleSampleController.js
@@ -0,0 +1,5 @@
+MyApp.controller('LocaleSampleController', function($scope, $locale){
+  console.log("Locale...", $locale);
+  $scope.myDate = Date.now();
+  $scope.myFormat = $locale.DATETIME_FORMATS.fullDate;
+});
