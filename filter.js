f7e5cabffeaf63e409cf5833cacc592c2e882667
Bhuvana <bhuvanatn@gmail.com>
2017-02-20 18:53:11 +1100

    custom filter created and used

---
+++ filter.js
@@ -0,0 +1,14 @@
+MyApp.filter('dura', function(){
+  return function(duration){
+    switch(duration){
+      case 1:
+      return 'Half Hour';
+      case 2:
+      return 'One Hour';
+      case 3:
+      return 'Half Day';
+      case 4:
+      return 'Full Day'
+    }
+  }
+});
